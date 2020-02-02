53661deb03e183979738e4729013cc99a63ffcc7
A D Vishnu Prasad <advishnuprasad@gmail.com>
2020-02-02 19:36:06 +0530

    Date change

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -115,9 +115,9 @@
      */
     simplyCountdown = function (elt, args) {
         var parameters = extend({
-                year: 2020,
-                month: 4,
-                day: 22,
+                year: 2015,
+                month: 6,
+                day: 28,
                 hours: 0,
                 minutes: 0,
                 seconds: 0,
